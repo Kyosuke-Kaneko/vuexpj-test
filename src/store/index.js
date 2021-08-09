3f707ab022f89e6b2d6160cb959f20311d94c56c
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2021-08-09 10:54:54 +0900

    test

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    count: 0,
   },
   mutations: {
+    incrementMutation(state, payload) {
+      console.log(payload);
+      state.count += payload
+    },
   },
   actions: {
+    incrementAction(context) {
+      setTimeout(function () { // １秒かかる通信
+        let data = 1;
+        console.log("waited a second");
+        context.commit("incrementMutation",data);
+      }, 1000);
+    },
   },
-  modules: {
-  }
-})
+});
